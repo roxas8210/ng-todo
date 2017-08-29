19ac8f86f8e06d20a94b3bb5b0ad0109a45fe483
roxas8210 <roxas8210@163.com>
2017-08-29 18:21:28 +0800

    添加todo service

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from './todo.model';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers: [ TodoService ]
 })
 export class TodoComponent implements OnInit {
   todos: Todo[] = [];
   desc: string = "";
 
-  constructor() { }
+  constructor( private service: TodoService) { }
 
   addTodo() {
-    this.todos.push({
-      id: 1,
-      desc: this.desc,
-      completed: false
-    });
+    this.todos = this.service.addTodo(this.desc);
     this.desc = "";
   }
 
